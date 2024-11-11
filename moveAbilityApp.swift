9855cf85e94483febd8aeb053c9aa2c2d7edf42c
PRAKASH DHANVIN <157555053+DhanvinPrakash@users.noreply.github.com>
2024-11-11 09:24:47 +0800

    Add files via upload

---
+++ moveAbilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  moveAbilityApp.swift
+//  moveAbility
+//
+//  Created by dhanvin_macbook on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct moveAbilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
